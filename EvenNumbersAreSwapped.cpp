81f54a0ddd791c7ef39b2b92aaee5dbc85eaa952
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-11-04 20:38:40 +0300

    Add files via upload

---
+++ EvenNumbersAreSwapped.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i = 0;
+    cin >> n;
+    int array[100]{};
+
+    for (; i < n; i++)
+    {
+        cin >> array[i];
+    }
+    for (int j = i - 1; j >= 0; j--)
+    {
+        if (array[j] % 2 == 0 && array[j] != 0) cout << array[j] << " ";
+    }
+    system("pause");
+}
